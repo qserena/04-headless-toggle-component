5b9521c343c21affb288746c6965f2eb1657ee5b
Tom Serenander <tom@serenander.se>
2023-11-22 10:40:07 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Toggle from './Toggle/index'
+import { BsStar, BsStarFill } from 'react-icons/bs'
+
+function App() {
+	return (
+		<>
+			<Toggle>
+				<Toggle.Button>
+					<Toggle.On>
+						<BsStarFill className="star filled" />
+					</Toggle.On>
+					<Toggle.Off>
+						<BsStar className="star " />
+					</Toggle.Off>
+				</Toggle.Button>
+			</Toggle>
+		</>
+	)
+}
+
+export default App
---
+++ src/Toggle/ToggleButton.jsx
@@ -0,0 +1,14 @@
+/**
+ * Challenge:
+ *
+ * 1. Create a ToggleButton component
+ * 2. Receive the `toggle` function from context (2 parts!)
+ * 3. Render a div with an onClick that calls `toggle`
+ */
+import React from 'react'
+import { ToggleContext } from './Toggle'
+
+export default function ToggleButton({ children }) {
+	const { toggle } = React.useContext(ToggleContext)
+	return <div onClick={toggle}>{children}</div>
+}
